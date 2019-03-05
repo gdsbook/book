abf8a2fe049979be3833e45d188829afc6eb8264
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2019-03-05 23:15:39 +0000

    Adding more static content to be built into the site

---
+++ infrastructure/booksite/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Clean up earlier leftovers
+echo "Cleaning up existing tmp_book folder..."
+rm -rf host/tmp_book
+# Set up new jupyter-book project
+echo "Starting new jupyter-book project..."
+jupyter-book create host/tmp_book --demo
+echo "Copying files over book folder..."
+# Copy _config.ym
+cp host/infrastructure/booksite/_config.yml host/tmp_book/_config.yml
+# Copy notebooks/content
+rm -r host/tmp_book/content
+mkdir host/tmp_book/content
+cp -r host/notebooks/ host/tmp_book/content/notebooks/
+# Copy other contents of the book (intro, etc.)
+cp host/infrastructure/booksite/intro.md host/tmp_book/content/intro.md
+# TOC
+cp host/infrastructure/booksite/toc.yml host/tmp_book/_data/toc.yml
+# LICENSE
+# Code requirements
+# Bibliography
+# Build book
+echo "Building book..."
+jupyter-book build host/tmp_book
+echo "Building site HTML..."
+cd host/tmp_book && make site
+# Move over to docs folder to be served online
+echo "Moving build over to docs folder..."
+cd /home/jovyan
+rm -r host/docs
+mv host/tmp_book host/docs
+# Clean up
+echo "All done!"
+
